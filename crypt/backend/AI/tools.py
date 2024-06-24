d7c15fec8245b13b39695b30f0f944a2cc58d531
jondoescoding <jondidathing.jm@gmail.com>
2024-06-23 21:28:54 -0500

    historical data tools + data analysis

--- crypt/backend/AI/tools.py
+++ crypt/backend/AI/tools.py
@@ -2,10 +2,20 @@
 import os
 import requests
 from datetime import datetime
+from dotenv import load_dotenv
 
 # Langchain
 from langchain.pydantic_v1 import BaseModel, Field
 from langchain.tools import StructuredTool
+from langchain_experimental.utilities import PythonREPL
+from llm import LLM
+
+# Phidata
+from phi.assistant.python import PythonAssistant
+from phi.llm.openai import OpenAIChat
+
+# Loading the environmental variables from the containing folder
+load_dotenv()
 
 """
 FUNCTIONS
@@ -128,6 +138,12 @@ def get_derivative_data(derivative_data: str):
         return f"Failed to fetch data from the API."
 
 def get_trending_coins():
+    """
+    Fetches and processes trending coins data from the CoinGecko API, removing 'price_change_percentage_24h' from each coin's data.
+
+    Returns:
+        list: A list of dictionaries, each representing a trending coin with its data, excluding 'price_change_percentage_24h'.
+    """
     url = "https://api.coingecko.com/api/v3/search/trending"
     headers = {
         "accept": "application/json",
@@ -144,6 +160,14 @@ def get_trending_coins():
     return coins_data
 
 def get_trending_nfts():
+    """
+    Fetches and returns trending NFTs data from the CoinGecko API.
+
+    This function sends a GET request to the CoinGecko API to retrieve trending NFTs data. It sets the 'accept' header to 'application/json' to specify the expected response format and includes the API key in the 'x-cg-demo-api-key' header for authentication. The function then parses the response and returns a list of trending NFTs data. If no NFTs data is found, an empty list is returned.
+
+    Returns:
+        list: A list of dictionaries, each representing a trending NFT with its data.
+    """
     url = "https://api.coingecko.com/api/v3/search/trending"
     headers = {
         "accept": "application/json",
@@ -153,6 +177,14 @@ def get_trending_nfts():
     return data.get('nfts', [])
 
 def get_trending_categories():
+    """
+    Fetches and returns trending categories data from the CoinGecko API.
+
+    This function sends a GET request to the CoinGecko API to retrieve trending categories data. It sets the 'accept' header to 'application/json' to specify the expected response format and includes the API key in the 'x-cg-demo-api-key' header for authentication. The function then parses the response and returns a list of trending categories data. If no categories data is found, an empty list is returned.
+
+    Returns:
+        list: A list of dictionaries, each representing a trending category with its data.
+    """
     url = "https://api.coingecko.com/api/v3/search/trending"
     headers = {
         "accept": "application/json",
@@ -161,33 +193,109 @@ def get_trending_categories():
     data = fetch_data(url, headers)
     return data.get('categories', [])
 
+def get_historical_chart_data_by_id(coin_name: str, days: int, chart_data_type: str):
+    """
+    Gets the historical chart data of a coin including time in UNIX, price, market cap and 24hrs volume based on particular coin id from the CoinGecko API.
+
+    Args:
+        coin_name (str): The ID of the cryptocurrency coin for which to fetch historical data.
+        days (int): The number of days for which to fetch historical data.
+
+    Returns:
+        dict: A dictionary containing the historical data for the specified coin over the specified number of days. If no data is found, an empty dictionary is returned.
+    """
+    url = f"https://api.coingecko.com/api/v3/coins/{coin_name}/market_chart?vs_currency=usd&days={days}&interval=daily&precision=full"
+    headers = {
+        "accept": "application/json",
+        "x-cg-demo-api-key": os.getenv("COINGECKO_API_KEY")
+    }
+    response_data = fetch_data(url, headers)
+    
+    # Extract the requested data
+    if chart_data_type in response_data:
+        return {chart_data_type: response_data[chart_data_type]}
+    else:
+        return {"error": f"Data type '{chart_data_type}' not found in the response."}
+
+def get_historical_chart_data_by_id_timerange(coin_name: str, from_: int, to_: str):
+    url = f"https://api.coingecko.com/api/v3/coins/{coin_name}/market_chart/range?vs_currency=usd&from={from_}&to={to_}&precision=full"
+
+
+    headers = {
+        "accept": "application/json",
+        "x-cg-demo-api-key": os.getenv("COINGECKO_API_KEY")
+    }
+    data = fetch_data(url, headers)
+    return data
+
+def get_ohlc_chat_by_id(coin_name: str, days: int):
+    url = f"https://api.coingecko.com/api/v3/coins/{coin_name}/ohlc?vs_currency=usd&days={days}&precision=full"
+    
+    headers = {
+        "accept": "application/json",
+        "x-cg-demo-api-key": os.getenv("COINGECKO_API_KEY")
+    }
+    
+    data = fetch_data(url, headers)
+    
+    return data
+
 
 """
 CLASSES
 """
+
+# CoinInput class is used to define the input structure for coin-related operations.
+# It contains a single field 'coin_name' which should be the ID# of a coin.
 class CoinInput(BaseModel):
     coin_name: str = Field(description="Should be the ID# of a coin")
 
+# MoneyInput class is used to define the input structure for currency conversion operations.
+# It contains two fields: 'usd_coin_price' for the USD dollar value ($) of the coin and 'currency_to_convert_to' for the target currency.
 class MoneyInput(BaseModel):
     usd_coin_price: str = Field(description="Should be USD dollar value ($) of the coin")
     currency_to_convert_to: str = Field(description="This is the currency which the USD price is being converted TO")
-    
+
+# NFTInput class is used to define the input structure for NFT-related operations.
+# It contains a single field 'nft_data' which can be the name, symbol, or an id of a NFT collection as represented on the CoinGecko Website.
+# IDs of NFT collections on CoinGecko typically have hyphens. For example: pudgy-penguins.
 class NFTInput(BaseModel):
     nft_data: str = Field(description="This is either the name, symbol or an id of a NFT collection as represented on the CoinGecko Website. ID all have hypens. For example: pudgy-penguins")
-    
+
+# DerivativeInput class is used to define the input structure for derivative-related operations.
+# It contains a single field 'derivative_data' which can be the name or an id of a derivative from a specific exchange.
 class DerivativeInput(BaseModel):
     derivative_data: str = Field(description="This is the name or an id of a derivative from a specific exchange")
+
+class HistoricalData(BaseModel):
+    coin_name: str = Field(description="The ID # of a coin")
+    days: int = Field(description="Number of days for historical data")
+    chart_data_type: str = Field(description="What type of data is being asked for. Can ONLY be 3 options: prices, market_caps, volume (total). If the option given sounds close please change it to one of the 3 options. Example: the user gives 'price' it should be converted to prices ")
+
+class OHLCData(BaseModel):
+    coin_name: str = Field(description="The ID # of a coin")
+    days: int = Field(description="Number of days for historical data")
+
+class HistoricalTimeRangeData(BaseModel):
+    coin_name: str = Field(description="The ID # of a coin")
+    from_: str = Field(description="The date from which to start the historical data. Should be converted from natural language to UNIX timestamp")
+    to_: str = Field(description="The date which historical data should end at. Should be converted from natural language to UNIX timestamp")
+
+class AIPrompt(BaseModel):
+    message: str = Field(description="The text prompt for the AI to process")
 
 """
 TOOL CREATION (combination of classes and functions)
 """
 
+# This tool returns an error message if the user asks for more coins than 10.
 get_all_coins_tool = StructuredTool.from_function(
     func=get_all_coins,
     name="Get_ALL_Tokens",
     description="Returns an error message if the user asks for more coins than 10. For example: if the user asks for ALL tokens from a platform",
 )
 
+# This tool returns specific cryptocurrency coin data from the Coin Gecko platform.
 specific_coin_price_tool = StructuredTool.from_function(
     func=get_specific_coin_price,
     name="Specific_Tool_Data",
@@ -195,6 +303,7 @@ specific_coin_price_tool = StructuredTool.from_function(
     args_schema=CoinInput   
 )
 
+# This tool converts the given USD coin price to the specified currency using a conversion API.
 convert_coin_price_tool = StructuredTool.from_function(
     func=convert_coin_price,
     name="Convert_Coin_Price",
@@ -202,6 +311,7 @@ convert_coin_price_tool = StructuredTool.from_function(
     args_schema=MoneyInput
 )
 
+# This tool returns ALL the NFT Data of a given NFT collection.
 get_nft_data_tool = StructuredTool.from_function(
     func=get_nft_data,
     name="Get_NFT_Data",
@@ -209,6 +319,7 @@ get_nft_data_tool = StructuredTool.from_function(
     args_schema=NFTInput
 )
 
+# This tool returns a specific derivative and its respective data from the CoinGecko platform based on the provided information.
 get_derivative_data_tool = StructuredTool.from_function(
     func=get_derivative_data,
     name="Get_Derviative_Data",
@@ -216,6 +327,7 @@ get_derivative_data_tool = StructuredTool.from_function(
     args_schema=DerivativeInput
 )
 
+# This tool returns the trending coins data from the CoinGecko platform.
 get_trending_coins_tool = StructuredTool.from_function(
     func=get_trending_coins,
     name="Get_Trending_Coins",
@@ -223,6 +335,7 @@ get_trending_coins_tool = StructuredTool.from_function(
     args_schema=None
 )
 
+# This tool returns the trending NFTs data from the CoinGecko platform.
 get_trending_nfts_tool = StructuredTool.from_function(
     func=get_trending_nfts,
     name="Get_Trending_NFTs",
@@ -230,6 +343,7 @@ get_trending_nfts_tool = StructuredTool.from_function(
     args_schema=None
 )
 
+# This tool returns the trending categories data from the CoinGecko platform.
 get_trending_categories_tool = StructuredTool.from_function(
     func=get_trending_categories,
     name="Get_Trending_Categories",
@@ -237,6 +351,42 @@ get_trending_categories_tool = StructuredTool.from_function(
     args_schema=None
 )
 
+# Python Tool
+python_assistant = PythonAssistant(
+    llm=OpenAIChat(model="gpt-3.5-turbo", api_key=os.getenv("OPENAI_TOKEN")),
+    pip_install=True,
+    show_function_calls=True,
+    run_code=True
+)
+
+python_assistant_tool = StructuredTool.from_function(
+    func=python_assistant.print_response,
+    description="A Python shell for data analysis, predictions, converting natural language dates to UNIX timestamps and machine learning. Use this to execute python commands. Input should be a valid python command.",
+    name="python_assistant",
+    args_schema=AIPrompt
+)
+
+# Get Historical Dat By ID Tool
+get_historical_data_by_id_tool = StructuredTool.from_function(
+    func=get_historical_chart_data_by_id,
+    name="Get_Historical_Data_By_ID",
+    description="Returns historical data for a given cryptocurrency by its ID.",
+    args_schema=HistoricalData
+)
+
+get_historical_data_within_timeRange_by_id_tool = StructuredTool.from_function(
+    func=get_historical_chart_data_by_id_timerange,
+    name="Get_Historical_Data_By_ID_TimeRange",
+    description="Returns historical data for a given cryptocurrency by its ID within a specified UNIX timestamped time range.",
+    args_schema=HistoricalTimeRangeData
+)
+
+ohlc_tool = StructuredTool.from_function(
+    func=get_ohlc_chat_by_id,
+    name="OHLC_Tool",
+    description="Returns the Open, High, Low, Close data for a given cryptocurrency over a specified number of days.",
+    args_schema=OHLCData
+)
 
 
 # ----------TESTING----------------
@@ -245,7 +395,8 @@ from langchain_core.prompts import ChatPromptTemplate
 from llm import LLM
 
 
-tools = [get_all_coins_tool, specific_coin_price_tool, convert_coin_price_tool, get_nft_data_tool, get_derivative_data_tool, get_trending_coins_tool, get_trending_nfts_tool, get_trending_categories_tool]
+tools = [get_all_coins_tool, specific_coin_price_tool, convert_coin_price_tool, get_nft_data_tool, get_derivative_data_tool, get_trending_coins_tool, get_trending_nfts_tool, get_trending_categories_tool, get_historical_data_by_id_tool, ohlc_tool, python_assistant_tool]
+
 prompt = ChatPromptTemplate.from_messages(
     [
         (
@@ -262,4 +413,8 @@ agent = create_tool_calling_agent(LLM.gpt3_5, tools, prompt)
 
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 
-print(agent_executor.invoke({"input": "what is the name of the number 1 trending category? Give me an overview report on it."}))
+print(agent_executor.invoke(
+    {
+        "input": "Using ALL of the last 30 days of prices for Bitcoin predict what the prices will be 7 days from now. Do not show any plots. ONLY print the 7 days values by their date."
+    })
+    )
